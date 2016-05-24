df410a61a78e9e9eb878857edcfedeac104ae6ea
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-05-24 15:49:36 -0500

    add skip filter to authenticate user

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
-
+  skip_before_filter :authenticate_user, :except => [:index, :new, :create, :update]
   # GET /images
   # GET /images.json
   def index
