d3df44e251d6a1b85b091d410977b0a4ce18e342
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-05-24 13:54:23 -0500

    removed add_files_to_documents migration

--- db/migrate/20160418004206_add_files_to_documents.rb
+++
@@ -1,9 +0,0 @@
-class AddFilesToDocuments < ActiveRecord::Migration
-  def up
-    add_attachment :documents, :document_file
-  end
-
-  def down
-    remove_attachment :documents, :document_file
-  end
-end
