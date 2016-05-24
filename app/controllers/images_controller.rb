a8b16fba0ce6f75646ea119a35eaf07d6acd4143
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-05-24 15:39:35 -0500

    added devise exception for :show, added find by token to show

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -14,6 +14,7 @@ class ImagesController < ApplicationController
   # GET /images/1
   # GET /images/1.json
   def show
+    @image = Image.find_by(token: params[:token], id: params[:id])
   end
 
   def share
