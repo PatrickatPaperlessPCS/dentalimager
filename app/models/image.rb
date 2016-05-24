ddb72faed6c8fef94ba268e9b87d5aec5159f653
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-05-24 15:19:01 -0500

    added proper routing back to image model, set default url option to demo.dentalimager.com

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
 require 'aws-sdk'
 
 
-	has_attached_file :image_file, url: "/images/:id/:filename", path:"/images/:id/:filename"
+	has_attached_file :image_file, url: "/esignhealth/images/:id/:filename", path:"/esignhealth/images/:id/:filename"
 	# validates_attachment_file_name :image_file, matches: [/png\Z/, /jpeg\Z/, /tiff\Z/, /bmp\Z/, /jpg\Z/]
    validates_attachment_content_type :image_file, content_type: /\Aimage\/.*\Z/
    # validates :image_file, :presence => true
